6e5777611b32660015056b0529290e2a5522b90f
Joshua Yeboah <joshua.yeboah@uconn.edu>
2025-06-12 22:51:01 -0400

    added backend dependencies

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected ✅'))
+.catch(err => console.error('MongoDB connection error:', err));
